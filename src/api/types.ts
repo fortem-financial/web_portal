ebaac9fe62b2f50bf572ca7e86187c4785b76f3f
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2022-06-29 20:22:22 -0300

    Initial commit

---
+++ src/api/types.ts
@@ -0,0 +1,29 @@
+export interface Config {
+	api: {
+		authzURL: string;
+		transactionURL: string;
+		switchURL: string;
+		downstreamURL: string;
+		ieoURL: string;
+	};
+	minutesUntilAutoLogout?: string;
+	rangerReconnectPeriod?: string;
+	withCredentials: boolean;
+	storage: {
+		defaultStorageLimit?: number;
+		orderBookSideLimit?: number;
+	};
+	gaTrackerKey?: string;
+	msAlertDisplayTime?: string;
+	incrementalOrderBook: boolean;
+	finex: boolean;
+	isResizable: boolean;
+	isDraggable: boolean;
+	languages: string[];
+	sessionCheckInterval: string;
+	balancesFetchInterval: string;
+	passwordEntropyStep: number;
+	showLanding: boolean;
+	sentryEnabled?: boolean;
+	kycSteps?: string[];
+}
---
+++ src/translations/index.ts
@@ -0,0 +1,9 @@
+import { en } from './en';
+
+export type LangType = typeof en;
+
+export const languageMap = {
+	default: en,
+	en,
+	
+};
